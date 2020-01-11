4f130dd3fdab5c03f10286407787c42e94609708
wic0144 <Arm_ruttanard@hotmail.com>
2020-01-12 06:18:40 +0700

    addnewfile

--- src/src/Employees.java
+++ src/src/Employees.java
@@ -1,2 +1,46 @@
 public class Employees {
+
+
+        public String name;
+        public String id;
+        private Salary sl;
+
+        public void setSalary(Salary s) {
+            this.sl=s;
+        }
+        public Salary getSalary() {
+            return sl;
+        }
+
+        public Employees(String name, String id) {
+            this.name = name;
+            this.id = id;
+        }
+
+
+        public String getName() {
+            return name;
+        }
+
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        public String getId() {
+            return id;
+        }
+
+
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Name %s Salary+commission+bonus %.2f",this.name,sl.getSarary());
+        }
+
 }
---
+++ src/src/Salary.java
@@ -0,0 +1,43 @@
+public class Salary {
+
+        public double salary;
+        public double commission;
+        public int sale;
+
+        public int getSale() {
+            return sale;
+        }
+
+        public void setSale(int sale) {
+            this.sale = sale;
+        }
+
+        public Salary(double salary) {
+            this.salary = salary;
+        }
+
+
+
+        public double getSarary() {
+            return salary;
+        }
+
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+
+
+        public double getCommission() {
+            return commission;
+        }
+
+
+
+        public void setCommission(double commission) {
+            this.commission = commission;
+        }
+
+
+}
